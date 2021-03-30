eea1ea9eec5c27ad03e8375f9eb20a730d361097
Jang Haemin <io@jhaemin.com>
2021-03-31 01:28:59 +0900

    Improve performance (fix #1, fix #2 again)

--- src/index.js
+++ src/index.js
@@ -3,54 +3,36 @@
  * For example, "muyaho~" means "Hello world" or "Good morning" or whatever.
  */
 
+import muyahoImage from './muyaho.png'
+import muyahoAudio from './muyaho.mp3'
+
+const AudioContext = window.AudioContext || window.webkitAudioContext
+const audioCtx = new AudioContext()
+
 const audios = []
 const audiosCount = 10
 
 for (let i = 0; i < audiosCount; i += 1) {
-  const audio = new Audio('https://raw.githubusercontent.com/jhaemin/muyaho/main/src/muyaho.mp3')
-  audio.volume = 0
-  audio.play()
+  const audio = new Audio(muyahoAudio)
   audios.push(audio)
 }
 
 let nextAudioIndex = 0
-
-class Muyaho {
-  constructor({ x, y }) {
-    const img = document.createElement('img')
-    img.classList.add('muyaho-img')
-    img.src = 'https://raw.githubusercontent.com/jhaemin/muyaho/main/src/muyaho.png'
-    img.style.left = x + 'px'
-    img.style.top = y + 'px'
-
-    document.body.appendChild(img)
-    audios[nextAudioIndex].currentTime = 0
-    audios[nextAudioIndex].volume = 1
-    audios[nextAudioIndex].play()
-    nextAudioIndex = (nextAudioIndex + 1) % audiosCount
-
-    img.getBoundingClientRect()
-    img.style.transform = 'translateX(-50%) translateY(-50%) scale(5)'
-
-    setTimeout(() => {
-      img.style.opacity = '0'
-
-      setTimeout(() => {
-        img.remove()
-      }, 300)
-    }, 700)
-  }
-}
+const imageWidth = 300
+const imageHeight = 222
+const halfOfImageWidth = imageWidth / 2
+const halfOfImageHeight = imageHeight / 2
 
 const muyahoStyle = document.createElement('style')
 muyahoStyle.innerHTML = /* css */ `
     .muyaho-img {
-      width: 100px;
+      width: ${imageWidth}px;
       height: auto;
       transition: transform 1.5s ease-out, opacity 300ms ease;
       position: fixed;
       z-index: 9999999999;
-      transform: translateX(-50%) translateY(-50%);
+      transform-origin: center center;
+      transform: scale(0.5);
       user-select: none;
       pointer-events: none;
       will-change: transform opacity;
@@ -60,5 +42,28 @@ muyahoStyle.innerHTML = /* css */ `
 document.head.appendChild(muyahoStyle)
 
 window.addEventListener('pointerdown', (e) => {
-  new Muyaho({ x: e.pageX, y: e.pageY })
+  const img = document.createElement('img')
+  img.classList.add('muyaho-img')
+  img.src = muyahoImage
+  img.style.left = e.pageX - halfOfImageWidth + 'px'
+  img.style.top = e.pageY - halfOfImageHeight + 'px'
+
+  img.getBoundingClientRect()
+
+  document.body.appendChild(img)
+
+  audios[nextAudioIndex].currentTime = 0
+  audios[nextAudioIndex].play()
+  nextAudioIndex = (nextAudioIndex + 1) % audiosCount
+
+  img.getBoundingClientRect()
+  img.style.transform = 'scale(1.5)'
+
+  setTimeout(() => {
+    img.style.opacity = '0'
+
+    setTimeout(() => {
+      img.remove()
+    }, 300)
+  }, 700)
 })
